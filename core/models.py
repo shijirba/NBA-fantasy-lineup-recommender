06876ac24a49b9b29a5a5619b1bb23e1455ddc5c
shijirba <78646055+shijirba@users.noreply.github.com>
2023-09-09 22:09:43 +0800

    Update models.py - including player rank table

--- core/models.py
+++ core/models.py
@@ -39,3 +39,10 @@ class Player(models.Model):
 
     def __str__(self):
         return self.name
+        
+class Rank(models.Model):
+    name = models.CharField(max_length=255)
+    rank = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
