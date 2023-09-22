29aecd3edae020de50c45eeaa4f160a6f8929612
shijirba <78646055+shijirba@users.noreply.github.com>
2023-09-22 17:34:09 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import player_list, find, player_compare
+from .views import player_list, find, compare, compare_player, rank, prediction
 
 urlpatterns = [
     path('', player_list, name='home'),
-    path('compare', player_compare, name='compare'), 
+    path('compare', compare, name='compare'), 
+    path('compare-player', compare_player, name='compare-player'), 
     path('find', find, name='find'), 
+    path('rank', rank, name='rank'), 
+    path('prediction', prediction, name='prediction'), 
 ]
